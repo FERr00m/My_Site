a917a75f64aac294932c250852ece814eae63065
Alex Melnikov <alexmlnkv@gmail.com>
2021-06-27 21:47:54 +0300

    начал делать светлую тему

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,10 +1,29 @@
 const header = () => {
   const dayNight = document.getElementById('toggle--daynight');
 
+  //elements for toggle light/dark theme
+  const arr = [];
+  const header = document.querySelector('.header'),
+    hero = document.querySelector('.hero'),
+    heroWrap = hero.querySelector('.hero-wrap'),
+    contacts = heroWrap.querySelector('.contacts');
+
+  arr.push(document.body);
+  arr.push(header);
+  arr.push(hero);
+  arr.push(heroWrap);
+  arr.push(contacts);
+    
+
+  
   dayNight.addEventListener('click', (e) => {
     e.target.checked ?
-      document.body.style.backgroundColor = '#fff8f2' :
-      document.body.style.backgroundColor = '#000'
+    arr.forEach(item => {
+      item.classList.add('light-theme')
+    }) :
+    arr.forEach(item => {
+      item.classList.remove('light-theme')
+    });
     
   })
 
